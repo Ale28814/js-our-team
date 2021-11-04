6a0b9e8781fc202bc5607ba825dc82a7f75ca27f
Ale28814 <alened928@gmail.com>
2021-11-04 16:15:12 +0100

    add array object

---
+++ main.js
@@ -0,0 +1,40 @@
+//1 creo l'arrei d'oggetto per i dati dei collaboratori
+
+const collab = [
+    {
+       name: 'Angela Caroll',
+       role: 'Chef Editor',
+       profile: 'img/angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        profile: 'img/walter-gordon-office-manager.jpg',
+    },
+
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        profile: 'img/angela-lopez-social-media-manager.jpg',
+    },
+
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        profile: 'img/scott-estrada-developer.jpg',
+    },
+
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        profile: 'img/barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+console.table(collab);
+
+//2 stampo a schermo sotto forma di card
+
+
+
